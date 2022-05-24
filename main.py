6e4de4c6b803a1479b271baf76b4f07531f670c3
l1st1k <listik.mp@gmail.com>
2022-05-24 15:57:23 +0300

    First commit (just elo showing)

---
+++ main.py
@@ -0,0 +1,34 @@
+import telebot
+import requests
+
+tel_API = "5329906325:AAH8pfRyRHvdpYcZvvFUvvgwbyHLUCFzmRM"
+faceit_API = "ab021e23-0297-4052-8094-751f00896b81"
+
+match_id_example = '1-1f4bb450-998d-45f2-a664-6b850f271c51'
+
+bot = telebot.TeleBot(tel_API)
+headers = {"Authorization": f"Bearer {faceit_API}", "accept": "application/json"}
+
+@bot.message_handler(commands=['start','help'])
+def start(message):
+    bot.send_message(message.chat.id, 'Privet, klounich')
+
+@bot.message_handler(commands=['elo'])
+def elo(message):
+    if (len(message.text)<5 or message.text.count(' ') == 0):
+        bot.send_message(message.chat.id, "Please follow the form:\n<b>/elo FaceitNickname</b>", parse_mode='html')
+    else:
+        user_name = message.text[5:]
+        response = requests.get(f"https://open.faceit.com/data/v4/players?nickname={user_name}", headers=headers)
+        if (response.status_code == 200):
+            user_elo = response.json()["games"]["csgo"]["faceit_elo"]
+            user_level = response.json()["games"]["csgo"]["skill_level"]
+            bot.send_message(message.chat.id, f'Player: {user_name}\nelo: {user_elo}\nlevel: {user_level}')
+        else:
+            bot.send_message(message.chat.id, "<b>404</b> <i>Not Found</i>", parse_mode='html')
+
+
+
+bot.polling(none_stop=True)
+
+# user_name, nickname, elo, matches to database
